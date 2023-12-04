115a4f4a1aa6532fa8044b1432aab6f63f9702ec
Vanita Badoni <vanita.badoni@evontech.com>
2023-12-04 11:41:57 +0530

    New changes

--- Scoping-practice/script.js
+++ Scoping-practice/script.js
@@ -66,7 +66,6 @@ const restaurant ={
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
     // new object
     openingHours: {
         thu:{
@@ -83,29 +82,100 @@ const restaurant ={
         }
 
     },
-
-    
     // new function
-    order: function (starterIndex, mainIndex){
+    order: function (starterIndex=2, mainIndex=2){
+        console.log(this.starterMenu[starterIndex]);
+        console.log(this.mainMenu[mainIndex]);
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+        
     },
+    // adding another function
 
-
+    orderDelivery: function(starterIndex =1, mainIndex=0, time='20:00', address= "Via del Sole, 21"){
+        console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and
+         ${this.mainMenu[mainIndex]} 
+        will be delivered to ${address} at ${time}`
+        );
+    }
 };
+restaurant.order({
+    starterIndex: 2,
+    mainIndex: 2
+});
+// Calling a function and giving the options
+// restaurant.orderDelivery({
+//     time: "22:30",
+//     address: "Via del Sole, 21",
+//     mainIndex: 2,
+//     starterIndex: 2
+// })
 
-const {name, openingHours, categories} = restaurant;
-console.log(name, openingHours, categories);
+restaurant.orderDelivery({
+    starterIndex: 1,
+    address: "Via del Sole, 21"
+});
 
-const {name: restaurantName, openingHours: hours, categories: tags } = restaurant;
-console.log(restaurantName, hours, tags);
 
-// Setting default values
-const {menu = [], starterMenu: starters = []} = restaurant;
-console.log(menu, starters)
 
-// Mutating variables 
-let a = 111;
-let b = 999;
-const obj = {a: 23, b: 7, c: 14};
-({a, b} = obj);
-console.log(a, b);
+//  restaurant.orderDelivery({
+//      time: '22:30',
+//      address: 'Via del Sole, 21',
+//      mainIndex: 2,
+//      starterIndex: 2,
+//  });
+
+ const { name, openingHours, categories} = restaurant;
+ console.log( name, openingHours, categories);
+
+ const {name: restaurantName, openingHours: hours, categories: tags } = restaurant;
+ console.log(restaurantName, hours, tags);
+
+ // Setting default values
+//  const {menu = [], starterMenu: starters = []} = restaurant;
+//  console.log(menu, starters)
+
+ // Mutating variables 
+ let a = 111;
+ let b = 999;
+ const obj = {a: 23, b: 7, c: 14};
+ // Destructuring Assignments
+ ({a, b} = obj);
+ console.log(a, b);
+
+ //Destructuring the nested object
+ const {fri: {open: o, close: c}} = openingHours;
+ console.log(o, c);
+
+//  REST PATTERN
+ const m = [1, 2, ...[3, 4]]; //spread operator, left side
+ const [p, q, ...others] = [1, 2, 3, 4, 5, 6];
+ console.log(p, q, others);
+
+
+// SPREAD OPERATOR
+const arr = [7, 8, 9];
+console.log(arr);
+// // We want to add some more numbers into an array mentioned above
+const newArr = [1, 2, ...arr];          //...arr is known as spread operator
+console.log(newArr)
+// //Similarly add Gnocci to the main menu in above example
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+   const mainMenuCopy = [...restaurant.mainMenu];
+
+   const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+   console.log(menu);
+
+// //Spread operators are applicable to all iterables. 
+// //Iterables: arrays, strings, maps, sets.Not objects.
+   const str = 'Jonas';
+   const letters = [...str, ' ', 'S.'];
+   console.log(...str);
+   console.log(letters);
+
+//Notes: Spread operator is similar to destructuring as it also helps out with getting elements out of an array.  The big
+//difference is it takes all the elements from an array and it also 
+
